436e2b06d6c2c74e67a366f7cbd7f964ab4191e5
Saukat Ali <esaukate@gmail.com>
2025-03-04 02:17:07 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  spotify: {
+    clientId: '7cdcf6566281445d8dd6383c5d7db58c',
+    clientSecret: '83dee8adabbb4c8fb5de17307abc67fd',
+    redirectUri: 'https://github.com/esaukate/Incorvi-Music/callback'
+  }
+};
